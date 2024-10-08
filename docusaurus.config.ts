4340d3f81165af2bdbf707c37e860b9b41793668
datlt2306 <datlt2306@gmail.com>
2024-10-08 13:58:12 +0700

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -138,6 +138,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['java', 'python', 'php'],
     },
   } satisfies Preset.ThemeConfig,
 };
